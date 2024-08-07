92f069d8c8d5ffcff0a7f0885e2c59b0c5e3740d
sunny kumar <128737486+SunnyKumar28@users.noreply.github.com>
2024-08-08 02:47:45 +0530

    Update Signup.jsx

--- Frontend/src/components/Signup.jsx
+++ Frontend/src/components/Signup.jsx
@@ -21,7 +21,8 @@ function Signup() {
       password: data.password,
     };
     await axios
-       .post("http://localhost:4001/user/signup", userInfo)
+       // .post("http://localhost:4001/user/signup", userInfo)
+      .post("https://mern-stack-fu32.onrender.com/user/signup", userInfo)
 
       .then((res) => {
         console.log(res.data);
